f8218c0085b756e310525b6c596ceb14af2671ec
alxshine <alexander.schloegl@student.uibk.ac.at>
2018-05-15 17:52:52 +0200

    Finished the corr.py
    Will check it as soon as i get the key bytes from Sebastian.

--- picoscope/corr.py
+++ picoscope/corr.py
@@ -11,22 +11,29 @@ def popcount(x):
     return bin(x).count('1')
 
 try:
-    corrs = np.load('corrs.npy')
+    print("Trying to load correlation file")
+    corrs = np.load("corr_{}".format(filename))
 except IOError:
+    print("Not found, generating one")
     try:
+        print("Loading traces")
         traces = np.load(filename)
     except IOError:
         print("Could not find file {}".format(filename))
         sys.exit(1)
 
-    plaintexts = np.repeat([0xf,0x1],(traces.shape[0]/2))
-    candidates = range(1<<4)
+    pt_range = range(1<<8)
+    plaintexts = np.repeat(pt_range,(traces.shape[0]/len(pt_range)+1))[:traces.shape[0]]
+    print(plaintexts.shape)
+    candidates = range(1<<8)
     hypopowcons = np.zeros((len(plaintexts), len(candidates)))
+    print("Generating hypothetical power consumptions")
     for i in range(len(plaintexts)):
         for j in range(len(candidates)):
             hypopowcons[i,j] = popcount(plaintexts[i]^candidates[j])
 
     corrs = np.zeros((len(candidates), traces.shape[1]))
+    print("Generating correlation coefficients")
     for i in candidates:
         print("candidate {}".format(i))
         for j in range(traces.shape[1]):
@@ -34,9 +41,12 @@ except IOError:
             corrs[i,j]  = (np.corrcoef(traces[:,j], hypopowcons[:,i]))[0,1]
         print()
 
-    np.save('corrs.npy', corrs)
+    np.save("corr_{}".format(filename), corrs)
 
 print(corrs.shape)
+peaks = corrs.max(axis=1)
+print(peaks.shape)
+print("Most probable key: {}".format(peaks.argmax()))
 plt.figure()
-plt.plot(corrs[0])
+plt.plot(peaks)
 plt.show()
