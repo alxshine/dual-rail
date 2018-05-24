9331be4e5b840d03622c508b4ee9ba906f209910
alxshine <alexander.schloegl@student.uibk.ac.at>
2018-05-24 19:56:15 +0200

    Made the generator more flexible
    Also generated some look up tables

--- generator/generate.py
+++ generator/generate.py
@@ -26,10 +26,10 @@ def testEncodeDecode(radix):
     for src in range(2**radix):
         assert (decode(encode(src, radix), radix) == src)
 
-def generateLUT(t, radix):
-    print("#ifndef LUT")
-    print("#define LUT")
-    print("{} lut[]".format(t), end='')
+def generateLUT(name, t, radix, operator):
+    print("#ifndef {}".format(name.upper()))
+    print("#define {}".format(name.upper()))
+    print("{} {}[]".format(t, name), end='')
     print(" = {")
     for i in range(2**(2*radix)):
         lh = decode(i, radix)
@@ -38,11 +38,12 @@ def generateLUT(t, radix):
         for j in range(2**(2*radix)):
             rh = decode(j, radix)
             
-            value = encode(lh ^ rh, radix)
+            value = encode(operator(lh, rh), radix)
             print("{}, ".format(value), end='')
         
         print()
     print("};")
     print("#endif")
 
-generateLUT("char", 4)
+op = lambda a,b: (int(a)*int(b))&255
+generateLUT("d_mult", "char", 4, op)
