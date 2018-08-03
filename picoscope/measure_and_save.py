aaaaa82f17720451aa414c186480aea0441dd53f
alxshine <alexander.schloegl@student.uibk.ac.at>
2018-08-03 11:05:24 +0200

    Fixed the test.py and measure_and_save.py
    They now both work with the new pico_python version.

--- picoscope/measure_and_save.py
+++ picoscope/measure_and_save.py
@@ -1,50 +1,43 @@
-#!/usr/bin/python2
-
-from picosdk import ps3000a as pico
 import matplotlib.pyplot as plt
 import numpy as np
-import sys
+import time
+import datetime
 
-if __name__ == "__main__":
-    ps = pico.Device()
-    status = ps.open_unit()
-    if status == ps.m.pico_num("PICO_OK"):
-        """configure channels"""
-        s, stateA = ps.get_channel_state(channel=ps.m.Channels.A)
-        stateA.enabled = True
-        stateA.range = ps.m.Ranges.r5v
-        s, stateB = ps.get_channel_state(channel=ps.m.Channels.B)
-        stateB.enabled = True
-        stateB.range = ps.m.Ranges.r200mv
-        status = ps.set_channel(channel=ps.m.Channels.A, state=stateA)
-        status = ps.set_channel(channel=ps.m.Channels.B, state=stateB)
+from picoscope import ps3000a
 
-        ps.set_simple_trigger(True, ps.m.Channels.A, 0.6, ps.m.ThresholdDirections.rising)
+ps = ps3000a.PS3000a()
+print(ps.getAllUnitInfo())
 
-        """ config """
-        num_runs = 1
-        num_traces = 10000
-        num_samples = 2000
-        path = "run_{:02}.npy"
+print("Setting channel A to DC 5V")
+ps.setChannel(channel="A", coupling="DC", VRange=5)
+print("Setting channel B to DC 1V")
+ps.setChannel(channel="B", coupling="DC", VRange=1)
+print("Setting trigger on channel A for 2V")
+ps.setSimpleTrigger("A", threshold_V=2)
+print()
 
-        if status == ps.m.pico_num("PICO_OK"):
-            s, index = ps.locate_buffer(channel=ps.m.Channels.B,
-                                        samples=num_samples,
-                                        segment=0,
-                                        mode=ps.m.RatioModes.raw,
-                                        downsample=0)
+num_plaintexts = 16
+captures_per_plaintext = 20
+n_captures = num_plaintexts * captures_per_plaintext
+print("Capturing {} plaintexts {} times each, resulting in {} traces total".format(num_plaintexts, captures_per_plaintext, n_captures))
+sample_interval = 10e-9 #10ns sampling interval
+sample_duration = 100e-6
+print("Sampling interval {}ns, sample duration {}ms".format(sample_interval*1e9, sample_duration*1e6))
 
-            for r in range(num_runs):
-                all_data = np.zeros((num_traces, num_samples))
-                for i in range(num_traces):
-                    print("{}/{}".format(i,num_traces))
-                    status = ps.collect_segment(segment=0, interval=50)
-                    if status == ps.m.pico_num("PICO_OK"):
-                        status, all_data[i] = ps.get_buffer_volts(index=index)
-                    else:
-                        print(ps.m.pico_tag(status))
-                        sys.exit(-1)
+ps.setSamplingInterval(sample_interval, sample_duration)
 
-                np.save(path.format(r), all_data)
+samples_per_segment = ps.memorySegments(n_captures)
+ps.setNoOfCaptures(n_captures)
 
-    print ps.m.pico_tag(status)
+print("Starting block capture")
+ps.runBlock()
+ps.waitReady()
+print("Block capture done")
+data, numSamplesReturned, _ = ps.getDataRawBulk(channel='B')
+print("{} samples returned".format(numSamplesReturned))
+print("Data.shape: {}".format(data.shape))
+print("All voltage data equal: {}".format(np.all(data == data[0][0])))
+ps.stop()
+filename = "{}_ptxts_{}.npy".format(num_plaintexts, datetime.datetime.today().strftime("%Y-%m-%d"))
+print("Saving to {}".format(filename))
+np.save(filename, data)
--- picoscope/test.py
+++ picoscope/test.py
@@ -5,76 +5,38 @@ import time
 from picoscope import ps3000a
 
 ps = ps3000a.PS3000a()
+print(ps.getAllUnitInfo())
 
-# now = time.strftime("%Y%m%d_%H%M%S")
-# filename = "sweep_" + now + ".swp"
-# output_file = open(filename, "wb")
+print("Setting channel A to DC 5V")
+ps.setChannel(channel="A", coupling="DC", VRange=5)
+print("Setting channel B to DC 1V")
+ps.setChannel(channel="B", coupling="DC", VRange=1)
+print("Setting trigger on channel A for 2V")
+ps.setSimpleTrigger("A", threshold_V=2)
+print()
 
-c = 3e8
-
-# rapid block mode
-
-ps.setChannel(channel="A", coupling="DC", VRange=1)
-ps.setChannel(channel="B", coupling="DC", VRange=0.5)
-
-n_captures = 1000
+num_plaintexts = 16
+captures_per_plaintext = 20
+n_captures = num_plaintexts * captures_per_plaintext
+print("Capturing {} plaintexts {} times each, resulting in {} traces total".format(num_plaintexts, captures_per_plaintext, n_captures))
 sample_interval = 10e-9 #10ns sampling interval
-sample_duration = 50e-6
+sample_duration = 100e-6
+print("Sampling interval {}ns, sample duration {}ms".format(sample_interval*1e9, sample_duration*1e6))
 
 ps.setSamplingInterval(sample_interval, sample_duration)
-ps.setSimpleTrigger("A", threshold_V=0.2)
 
 samples_per_segment = ps.memorySegments(n_captures)
 ps.setNoOfCaptures(n_captures)
 
+print("Starting block capture")
 ps.runBlock()
 ps.waitReady()
-data, _, _ = ps.getDataRawBulk(channel='B')
-print(data.shape)
+print("Block capture done")
+data, numSamplesReturned, _ = ps.getDataRawBulk(channel='B')
+print("{} samples returned".format(numSamplesReturned))
+print("Data.shape: {}".format(data.shape))
+print("All voltage data equal: {}".format(np.all(data == data[0][0])))
+ps.stop()
 for row in data:
     plt.plot(row)
 plt.show()
-
-# num_captures = 10
-# num_samples = 1000
-# data = np.zeros((num_captures, num_samples))
-# for i in range(num_captures):
-    # ps.runBlock()
-    # ps.waitReady()
-
-    # data[i] = ps.getDataV('B', numSamples=num_samples)
-    # plt.plot(data[i])
-
-# for i in range(n_captures):
-#     ps._lowLevelSetDataBuffer(ps.CHANNELS["A"],
-#         data[i, :], 0, i)
-
-# # t2 = time.time()
-# nsamples = c_int32(ps.noSamples)
-# from_segment_index = 0
-# to_segment_index = n_captures - 1
-# downsample_ratio = 0
-# downsample_mode = 0
-# overflow = np.zeros(n_captures, dtype=np.int16)
-# overflow_ptr = overflow.ctypes.data_as(POINTER(c_int16))
-
-# m = ps.lib.ps3000aGetValuesBulk(c_int16(ps.handle),
-#         byref(nsamples),
-#         c_int16(from_segment_index),
-#         c_int16(to_segment_index),
-#         c_int32(downsample_ratio),
-#         c_int16(downsample_mode),
-#         overflow_ptr)
-# print m
-
-# ps.checkResult(m)
-
-# output_file.write(data)
-# t4 = time.time()
-# print "Time to write data to disk: ", str(t4 - t3)
-# output_file.close()
-
-plt.show()
-
-ps.close()
-
