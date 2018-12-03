393b4cd83a13ed2ea84703a0dd61bcd60c25acd9
Alex <alexander.schloegl@uibk.ac.at>
2018-12-03 18:06:55 +0100

    Added compilation for aes code

--- bare_bones/aes.c
+++ bare_bones/aes.c
@@ -3,21 +3,15 @@
 #include "plaintexts.h"
 #include "tinyAES.h"
 
-
 #define OUTPIN 6
 uint8_t key[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 uint8_t buf[16];
 
-int main() {
+void c_entry() {
   for (int i = 0; i < 5; ++i) {
-    const uint8_t *plaintext = (uint8_t *)plaintexts[i++];
-  	memcpy(printText, plaintext, 16);
-  	printText[16] = 0;
-  	printf("Plaintext %d: %s\n", i, printText);
-    AES128_ECB_encrypt(plaintext, key, buf);
-    memcpy(printText, buf, 16);
-    printf("Ciphertext: %s\n\n", printText);
+    const char *plaintext = plaintexts[i++];
+    printn_uart0(plaintext, 16);
+    AES128_ECB_encrypt((uint8_t *)plaintext, key, buf);
+    printn_uart0((char*)buf, 16);
   }
-
-  return 0;
 }
