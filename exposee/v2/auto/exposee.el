1fa5968bb434686928bb17957c015f5d6bc41237
Alex <alexander.schloegl@uibk.ac.at>
2019-01-14 18:27:38 +0100

    Added background chapter

--- exposee/v2/auto/exposee.el
+++ exposee/v2/auto/exposee.el
@@ -2,7 +2,7 @@
  "exposee"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("geometry" "a4paper")))
+                     '(("geometry" "a4paper") ("fontenc" "T1")))
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -12,13 +12,16 @@
     "tikz"
     "caption"
     "subcaption"
-    "arev")
+    "sansmathfonts"
+    "fontenc")
    (TeX-add-symbols
     "qemu"
     "dual"
-    "llvm")
+    "llvm"
+    "lc")
    (LaTeX-add-labels
     "sec:background"
+    "fig:llvm"
     "sec:methodology"
     "sec:related-work"
     "sec:difficulties")
---
+++ exposee/v2/auto/sources.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook
+ "sources"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "kocher1999differential"
+    "sokolov2005design"
+    "bellard2005qemu"
+    "messerges2000securing"
+    "lattner2010llvm"
+    "lattner2004llvm"))
+ :bibtex)
+
