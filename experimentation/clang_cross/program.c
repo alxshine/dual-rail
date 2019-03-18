b561a20d05b094cba5223e0d05fe78869aed52af
Alexander Schlögl <alexander.schloegl@student.uibk.ac.at>
2019-03-18 08:51:31 +0100

    Fixed the fizzbuzz example code

--- experimentation/clang_cross/program.c
+++ experimentation/clang_cross/program.c
@@ -6,6 +6,7 @@ char hexChars[16] = {'0', '1', '2', '3', '4', '5', '6', '7',
 void string_copy(char *source, char *dest, int n) {
   for (int i = 0; i < n; i++)
     dest[i] = source[i];
+  dest[n] = '\0';
 }
 
 int power(int base, int exp) {
@@ -26,9 +27,11 @@ int div(int a, int b) {
 
 void write_int(int source, char *dest) {
   int index = 0;
+  dest[index++] = '0';
+  dest[index++] = 'x';
   for (int i = 28; i >= 0; i -= 4) {
-    int bits = (((0xF) << 28) & source) >> i;
-    dest[index] = hexChars[bits];
+    int bits = (((0xF) << i) & source) >> i;
+    dest[index++] = hexChars[bits];
   }
   dest[index] = '\0';
 }
@@ -37,17 +40,15 @@ void c_entry() {
   char buffer[20];
   print_uart0("Beginning\0");
   for (int i = 0; i < 100; i++) {
-    /* if (i % 15 == 0) */
-    /*   string_copy("fizzbuzz", buffer, 8); */
-    /* else if (i % 5 == 0) */
-    /*   string_copy("buzz", buffer, 4); */
-    /* else if (i % 3 == 0) */
-    /*   string_copy("fizz", buffer, 4); */
-    /* else */
-    /*   write_int(i, buffer); */
+    if (i % 15 == 0)
+      string_copy("fizzbuzz", buffer, 8);
+    else if (i % 5 == 0)
+      string_copy("buzz", buffer, 4);
+    else if (i % 3 == 0)
+      string_copy("fizz", buffer, 4);
+    else
+      write_int(i, buffer);
 
-    /* print_uart0(buffer); */
-    write_int(i, buffer);
-    /*print_uart0(buffer);*/
+    print_uart0(buffer);
   }
 }
