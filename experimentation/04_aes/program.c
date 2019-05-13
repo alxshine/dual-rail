f670b511ef2b60afe7bf9fe2679094804d306538
Alexander Schlögl <alexander.schloegl@student.uibk.ac.at>
2019-05-13 10:38:52 +0200

    KeyExpansion now works

--- experimentation/04_aes/program.c
+++ experimentation/04_aes/program.c
@@ -8,15 +8,9 @@ void c_entry() {
   unsigned char ciphertext[16];
   unsigned char decrypted[16];
 
-  char buffer[16];
-  for (int i = 0; i < 256; ++i) {
-    char test = getSBoxValue(i);
-    write_int(test, buffer);
-    print_uart0(buffer);
-  }
-  /* printn_uart0((char *)plaintext, 16); */
-  /* AES128_ECB_encrypt(plaintext, key, ciphertext); */
-  /* printn_uart0((char *)ciphertext, 16); */
+  printn_uart0((char *)plaintext, 16);
+  AES128_ECB_encrypt(plaintext, key, ciphertext);
+  printn_uart0((char *)ciphertext, 16);
   /* AES128_ECB_decrypt(ciphertext, key, decrypted); */
   /* printn_uart0((char *)decrypted, 16); */
 }
