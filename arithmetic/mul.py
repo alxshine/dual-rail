73a076be414308ec31441f444e3e9ff1fd9f6bae
Alexander Schlögl <alexander.schloegl@student.uibk.ac.at>
2019-06-18 10:43:06 +0200

    Added direct multiplication

---
+++ arithmetic/mul.py
@@ -0,0 +1,13 @@
+from multiop import *
+
+m = MultiStepOperation([
+    Convert_1_2(1), #2
+    BinaryOperation(0,2, lambda x,y: (x*y) & 0xffffffff),#3
+    BinaryOperation(3,1, lambda x,y: x + (y << 16)), #4
+    UnaryOperation(4, lambda x: x + 0x00ff0000), #5
+    UnaryOperation(5, lambda x: x & 0x00ff00ff), #6
+])
+m.execute()
+incorrectResults = m.testCorrectness(lambda x,y: (x*y)&0xff)
+print(f"Number of incorrect results: {len(incorrectResults.keys())}")
+m.visualize("SUB")
