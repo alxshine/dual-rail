1a3a91e4e763be1de1b9e64f7fe6842a18287383
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2019-07-19 15:02:42 +0200

    Worked on some automation stuff

--- experimentation/11_aes_eval/program.c
+++ experimentation/11_aes_eval/program.c
@@ -86,12 +86,19 @@ void c_entry() {
   key[14] = '1';
   key[15] = '1';
 
+  char done[4];
+  done[0] = 'd';
+  done[1] = 'o';
+  done[2] = 'n';
+  done[3] = 'e';
+
   unsigned char ciphertext[16];
   unsigned char decrypted[16];
 
-  printn_uart0((char *)plaintext, 16);
+  /*printn_uart0((char *)plaintext, 16);*/
   AES128_ECB_encrypt(plaintext, key, ciphertext);
-  printn_uart0((char *)ciphertext, 16);
+  /*printn_uart0((char *)ciphertext, 16);*/
   AES128_ECB_decrypt(ciphertext, key, decrypted);
-  printn_uart0((char *)decrypted, 16);
+  /*printn_uart0((char *)decrypted, 16);*/
+  printn_uart0(done, 4);
 }
--- experimentation/11_aes_eval/startup.s
+++ experimentation/11_aes_eval/startup.s
@@ -2,4 +2,3 @@
 _Reset:
  LDR sp, =stack_top
  BL balanced_c_entry
- B .
