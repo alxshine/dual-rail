26e436a8c97141785f07c24696b0ff0a5aa4af1e
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2019-07-26 16:35:10 +0200

    added new boundaries, explored reasons for imbalances

--- experimentation/11_aes_eval/analysis/plot_imbalances.py
+++ experimentation/11_aes_eval/analysis/plot_imbalances.py
@@ -1,16 +1,29 @@
-plt.scatter(indices, pt, s=0.4)
+from boundaries import *
+from load import *
+
+plt.figure()
 plt.scatter(indices, pt, s=0.3)
-plt.scatter(indices[diff!=0], pt[diff!=0], s=0.3)
+plt.scatter(indices[diff!=0], pt[diff!=0], s=0.3, label='Points with nonzero HW difference')
+plt.legend()
+plt.show()
+
+plt.figure()
+indices = indices[diff!=0]
+pt = pt[diff!=0]
+
+plt.scatter(indices, pt, s=0.3)
+
 unbalances = np.loadtxt('unbalances.csv').astype(np.int).flatten()
 unbalances = np.unique(unbalances)
-unbalances.shape
-np.isin(pt, unbalances)
 plt.scatter(indices[np.isin(pt, unbalances)], pt[np.isin(pt,unbalances)],s=0.4)
+
 ldrb = np.loadtxt('ldrb.csv').flatten()
 ldrb = np.unique(ldrb)
-ldrb.shape
 plt.scatter(indices[np.isin(pt, ldrb)], pt[np.isin(pt,ldrb)], s=0.4)
-lnc = np.loadtext('lsr.csv').flatten()
-lsr = np.unique(np.loadtxt('lsr.csv').flatten())
-lsr.unique()
+
+lsr = np.loadtxt('lsr.csv').flatten()
+lsr = np.unique(lsr)
 plt.scatter(indices[np.isin(pt,lsr)], pt[np.isin(pt,lsr)], s=0.4)
+
+plt.legend()
+plt.show()
