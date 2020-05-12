a535fa3b0055e161cacb5f510ad8394ad1a82022
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-05-12 18:11:12 +0200

    Minor changes to evaluation script

--- experimentation/11_aes_eval/evaluate.sh
+++ experimentation/11_aes_eval/evaluate.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+# test if output directory exists
+# if ! [ -d /results ]; then
+# 	echo "/results not found, cannot save results"
+# 	exit 1
+# fi
+
 for i in {1..3}; do
 	echo `printf "Compiling binary number %03d" $i`
 	make clean
 	make
 
+	echo "Evaluating balanced"
 	mv balanced.elf program.elf
 	./script.exp &
 	sleep 1
 	gdb-multiarch --batch -q -x debug.gdb program.elf > `printf "balanced_%03d.log" $i`
 	wait
 
+	echo "Evaluating unbalanced"
 	mv unbalanced.elf program.elf
 	./script.exp &
 	sleep 1
@@ -18,4 +26,4 @@ for i in {1..3}; do
 	wait
 done
 
-tar czf plaintexts.txt *.log /output.tar.gz
+tar czf /dual-rail/output.tar.gz plaintexts.txt *.log
