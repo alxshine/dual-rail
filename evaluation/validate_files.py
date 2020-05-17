b3851e8b5c1a6bbf92486e8252ff9736597ef7e9
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-05-17 09:20:47 +0200

    Cleaned up file validation

--- evaluation/validate_files.py
+++ evaluation/validate_files.py
@@ -2,25 +2,46 @@ import pathlib
 import re
 import sys
 
+
 def validate_files(log_dir):
     path = pathlib.Path(log_dir)
+
+    print("Checking balanced logs")
     balanced_log_files = path.glob('balanced_*.log')
     filtered_balanced = filter_files(balanced_log_files)
+    if not check_equality(filtered_balanced):
+        print("There were errors during validation")
+        return False
 
+    print("Checking unbalanced logs")
+    unbalanced_log_files = path.glob('unbalanced_*.log')
+    filtered_unbalanced = filter_files(unbalanced_log_files)
+    if not check_equality(filtered_unbalanced):
+        print("There were errors during validation")
+        return False
+    return True
+
+def check_equality(filtered_logs):
     mnemonic_re = re.compile('(\d+\s*)(.*$)')
 
     all_equal = True
-    for lines in zip(*filtered_balanced):
-        mnemonics = [mnemonic_re.search(l)[2] for l in lines if mnemonic_re.match(l)]
+    for lines in zip(*filtered_logs):
+        mnemonics = [mnemonic_re.search(l)[2]
+                     for l in lines if mnemonic_re.match(l)]
 
         if any(m != mnemonics[0] for m in mnemonics[1:]):
-            print("ERROR: not all execution paths are equal.\nOffending lines:\n", file=sys.stderr)
+            print(
+                "ERROR: not all execution paths are equal.\nOffending lines:\n", file=sys.stderr)
             all_equal = False
             for l in lines:
                 print(l, file=sys.stderr)
 
     if all_equal:
         print("All control flows are equal!")
+        return True
+    else:
+        return False
+
 
 def filter_files(balanced_log_files):
     filtered_logs = []
