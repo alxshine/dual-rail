37442a0d04e32ff2dcda107248e549e4d030b45e
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2020-05-21 15:33:28 +0200

    Increase runs, add elf to output for code size

--- experimentation/11_aes_eval/evaluate.sh
+++ experimentation/11_aes_eval/evaluate.sh
@@ -6,24 +6,24 @@
 # 	exit 1
 # fi
 
-for i in {1..10}; do
+for i in {1..50}; do
 	echo `printf "Compiling binary number %03d" $i`
 	make clean
 	make
 
 	echo "Evaluating balanced"
-	mv balanced.elf program.elf
+	cp balanced.elf program.elf
 	./script.exp &
 	sleep 1
 	gdb-multiarch --batch -q -x balanced.gdb program.elf > `printf "balanced_%03d.log" $i`
 	wait
 
 	echo "Evaluating unbalanced"
-	mv unbalanced.elf program.elf
+	cp unbalanced.elf program.elf
 	./script.exp &
 	sleep 1
 	gdb-multiarch --batch -q -x unbalanced.gdb program.elf > `printf "unbalanced_%03d.log" $i`
 	wait
 done
 
-tar czf /dual-rail/output.tar.gz plaintexts.txt *.log
+tar czf /dual-rail/output.tar.gz plaintexts.txt *.log balanced.elf unbalanced.elf
